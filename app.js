f97d0975b97b903edcb87fe0e6dc0cc02d26afc2
jagtapprathamesh09-007 <jagtapprathamesh09@gmail.com>
2026-09-02 01:31:52 +0530

    Configure connect-mongo store for persistent cloud sessions in MongoDB Atlas

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const console = require("console");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -65,18 +66,31 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-const sessionOption ={
-    secret : "mysupersecretcode",
-    resave : false,
-    saveUninitialized : true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
-    }
+// Mongo Session Store Setup
+const store = (MongoStore.default || MongoStore).create({
+  mongoUrl: dburl,
+  crypto: {
+    secret: "mysupersecretcode",
+  },
+  touchAfter: 24 * 3600,
+});
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+const sessionOptions = {
+  store,
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
-app.use(session(sessionOption));
+app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
