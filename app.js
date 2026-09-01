a7b023201f2271b365a54079c82a026aba9886e8
jagtapprathamesh09-007 <jagtapprathamesh09@gmail.com>
2026-09-02 01:49:48 +0530

    Secure sensitive keys and secrets by moving them to .env

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = (MongoStore.default || MongoStore).create({
   mongoUrl: dburl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -80,7 +80,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret:  process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
